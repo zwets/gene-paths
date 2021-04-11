0feaa2250d0cb223f2231872bd796293bb0dd40e
Marco van Zwetselaar <io@zwets.it>
2021-04-12 00:23:58 +0300

    Release 1.0.0

--- src/gene-paths.cpp
+++ src/gene-paths.cpp
@@ -30,48 +30,68 @@
 #include "dijkstra.h"
 #include "utils.h"
 
-
 using namespace gene_paths;
-
 
 static const std::string USAGE(
 "Usage: gene-paths [OPTIONS] GFA_FILE FROM TO\n"
 "\n"
-"  Output the shortest path between locations FROM and TO in the genome\n"
+"  Find the shortest path between locations FROM and TO in the genome\n"
 "  assembly graph in GFA_FILE.\n"
 "\n"
-"  FROM and TO are specified as CTG[:BEG[:END]]][+-], where CTG is the\n"
-"  contig name, BEG and END the optional start and end positions on CTG,\n"
-"  and + or - the strand.\n"
+"  OPTIONS\n"
+"   -b, --bidir       search for TO both upstream and downstream of FROM\n"
+"   -f, --fasta FILE  read sequences for GFA_FILE from FILE\n"
+"   -v, --verbose     write detailed progress information to stderr\n"
+"   -h, --help        print this information and exit\n"
+"\n"
+"  The path search looks for TO downstream of FROM.  Use option -b/--bidir\n"
+"  to also search for a path that has TO upstream of FROM.  Both paths (if\n"
+"  any exist) will be reported.\n"
+"\n"
+"  FROM and TO are specified as CTG[:BEG[:END]]S, where CTG is the name of\n"
+"  the contig, BEG and END are the optional start and end positions on CTG,\n"
+"  and S is the mandatory strand identifier (+ or -).\n"
 "\n"
 "  BEG and END can be '$' to signify the end of CTG.  When END is omitted\n"
-"  it defaults to BEG, so the reference is a zero-length position.  When\n"
-"  BEG and END are both omitted, they default to 0 and $ respectively.\n"
+"  it defaults to BEG, so the reference is a (zero length) position.\n"
+"  When BEG and END are both omitted, they default to 0 and $ respectively,\n"
+"  so the reference is the whole contig.\n"
 "\n"
-"  OPTIONS\n"
-"   -f, --fasta FILE  read sequences for GFA_FILE from FILE\n"
-"   -b, --both        search for TO upstream of FROM as well as downstream\n"
-"   -v, --verbose     write detailed information to stderr\n"
-"   -h, --help        output this information and exit\n"
+"  STRAND and POSITION are interpreted as in GFA2:\n"
+"  - we define the data in the GFA or FASTA file to be the + strand,\n"
+"    and its reverse reverse complement the - strand;\n"
+"  - positions are in between bases, with 0 to the left of the sequence,\n"
+"    and $ to the right, $ being the sequence length;\n"
+"  - positions are interpreted before orienting the segment, so pos 0 is\n"
+"    at the upstream end of a segment, regardless of sign.\n"
 "\n"
-"  The default is to search for a path going downstream from FROM.  Use\n"
-"  option -b/--both to also search for a path with TO upstream of FROM.\n"
+"  TIP: 'gene-paths 1+ 1:0+' finds the shortest CYCLICAL path starting at\n"
+"  (the + strand of) contig 1: it starts out going across all of 1+, then\n"
+"  searches for a path ending at its upstream end.  Note how '1:$+ 1:0+'\n"
+"  is similar, but does not include contig 1 itself.\n"
 "\n"
-"  Strand and position are interpreted as in GFA2:\n"
-"   - we call the data as defined in the GFA/FASTA the + strand, and its\n"
-"     reverse complement the - strand\n"
-"   - positions are in between bases, with 0 to the left of the sequence,\n"
-"     and $ to the right, where $ is the sequence length\n"
-"   - positions are interpreted before orienting the segment, so pos 0 is\n"
-"     at the upstream end of a segment, regardless of sign.\n"
+"  Note: if you use '$' in FROM or TO you will likely need to quote it,\n"
+"  to prevent interpretation by your command shell.\n"
 "\n");
 
-static void usage_exit()
+static void usage_exit(int err = 1)
 {
-    std::cerr << USAGE;
-    std::exit(1);
+    (err ? std::cerr : std::cout) << USAGE;
+    std::exit(err);
 }
 
+static void write_path(const gfa::dijkstra& d)
+{
+    if (d.found_pix) {
+        std::cout << ">PATH ";
+        d.write_route(std::cout);
+        std::cout << " (length " << d.length() << ")";
+        std::cout << std::endl;
+
+        d.write_sequence(std::cout);
+        std::cout << std::endl;
+    }
+}
 
 int main (int /*argc*/, char *argv[])
 {
@@ -79,7 +99,8 @@ int main (int /*argc*/, char *argv[])
 
     std::string gfa_fname;
     std::string fna_fname;
-    bool both_dirs = false;
+    bool bidirectional = false;
+    bool furthest = false;
 
         // parse options
 
@@ -88,12 +109,15 @@ int main (int /*argc*/, char *argv[])
         if (!std::strcmp("-v", *argv) || !std::strcmp("--verbose", *argv)) {
             set_verbose(true);
         }
-        else if (!std::strcmp("-h", *argv) || !std::strncmp("--help", *argv, 6)) {
-            std::cout << USAGE;
-            return 0;
+        else if (!std::strcmp("-h", *argv) || !std::strcmp("--help", *argv)) {
+            usage_exit(0);
         }
-        else if (!std::strcmp("-b", *argv) || !std::strncmp("--both", *argv, 6)) {
-            both_dirs = true;
+        // option is parsed and executed, just not documented in --help yet
+        else if (!std::strcmp("-u", *argv) || !std::strcmp("--furthest", *argv)) {
+            furthest = true;
+        }
+        else if (!std::strcmp("-b", *argv) || !std::strncmp("--bidir", *argv, 7)) {
+            bidirectional = true;
         }
         else if ((!std::strcmp("-f", *argv) || !std::strcmp("--fasta", *argv)) && *++argv) {
             fna_fname = *argv;
@@ -115,12 +139,14 @@ int main (int /*argc*/, char *argv[])
     if (!*argv) usage_exit();
     std::string from_ref = *argv++;
 
-    if (!*argv) usage_exit();
-    std::string to_ref = *argv++;
+    std::string to_ref;
+    if (!furthest && *argv) 
+        to_ref = *argv++;
 
     if (*argv) usage_exit();
 
-        // read GFA (and FASTA) into graph
+        // read GFA (and FASTA) into graph,
+        // reserving 3 segs and 4 arcs for the targets (see paths.h)
 
     gfa::graph g;
 
@@ -132,65 +158,63 @@ int main (int /*argc*/, char *argv[])
             raise_error("failed to open file: %s", fna_fname.c_str());
         verbose_emit("reading FASTA from file: %s", fna_fname.c_str());
       
-        // reserve 3 segs and 4 arcs for FROM and TO (see paths.h)
         g = gfa::parse(gfa_file, fna_file, 3, 4);
     }
     else {
         g = gfa::parse(gfa_file, 3, 4);
     }
 
-        // add targets to the graph
+        // set up for program return value
+
+    bool success = true;
+
+        // create targets on the graph, and set the from
 
     gfa::target from(g), to(g);
 
     from.set(from_ref, gfa::target::START);
-    to.set(to_ref, gfa::target::END);
 
-        // call dijkstra
+        // create dijkstra algorithm and point it at the graph
 
     gfa::dijkstra dijkstra(g);
 
-    verbose_emit("searching shortest path: %s -> %s", from_ref.c_str(), to_ref.c_str());
+        // @TODO@ document the -u/--furthest option
+        // it finds the shortest path from FROM to every possible TO,
+        // then returns the longest of these shortest paths
 
-    bool fwd = dijkstra.shortest_path(from.p_arc(), to.p_arc());
-    if (fwd)
+    if (furthest) // find longest of all shortest paths from FROM
     {
-        std::cout << ">PATH ";
-        dijkstra.write_route(std::cout);
-        std::cout << " (length " << dijkstra.length() << ")";
-        std::cout << std::endl;
+        verbose_emit("searching furthest path from: %s", from_ref.c_str());
 
-        dijkstra.write_sequence(std::cout);
-        std::cout << std::endl;
+        dijkstra.furthest_path(from.p_arc());
+        write_path(dijkstra);
     }
+    else // find shortest path from FROM to TO
+    {
+        verbose_emit("searching shortest path: %s -> %s", from_ref.c_str(), to_ref.c_str());
 
-        // if bidirectional, call again with from and to swapped
+        to.set(to_ref, gfa::target::END);
+        
+        success = dijkstra.shortest_path(from.p_arc(), to.p_arc());
+        write_path(dijkstra);
 
-    bool rev = false;
-    if (both_dirs) {
-
-        from.set(to_ref, gfa::target::START);
-        to.set(from_ref, gfa::target::END);
-
-        verbose_emit("searching inverse path: %s -> %s", to_ref.c_str(), from_ref.c_str());
-
-        rev = dijkstra.shortest_path(from.p_arc(), to.p_arc());
-        if (rev)
+        if (bidirectional) // also find shortest path with TO upstream of FROM
         {
-            std::cout << ">PATH_REV ";
-            dijkstra.write_route(std::cout);
-            std::cout << " (length " << dijkstra.length() << ")";
-            std::cout << std::endl;
-            dijkstra.write_sequence(std::cout);
-            std::cout << std::endl;
+            verbose_emit("searching inverse path: %s -> %s", to_ref.c_str(), from_ref.c_str());
+
+            from.set(to_ref, gfa::target::START);
+            to.set(from_ref, gfa::target::END);
+
+            success |= dijkstra.shortest_path(from.p_arc(), to.p_arc());
+            write_path(dijkstra);
+        }
+
+        if (!success) {
+            std::cerr << "No path was found\n";
         }
     }
 
-    if (!(fwd || rev)) {
-        std::cerr << "No path was found\n";
-    }
-
-    return fwd || rev;
+    return success ? 0 : 1;
 }
 
 // vim: sts=4:sw=4:et:si:ai
